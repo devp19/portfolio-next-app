42ef59a3875c657f273d14191257b2ae69a7bd7c
Dev Patel <devptl841806@gmail.com>
2025-10-12 14:00:58 -0400

    typo

--- next-portfolio/src/app/innovation/page.tsx
+++ next-portfolio/src/app/innovation/page.tsx
@@ -116,7 +116,7 @@ export default function ResDexPage() {
       announcements: [
         {
           tag: "YC",
-          title: "Offer Recieved",
+          title: "Offer Received",
         }
       ],
     },
